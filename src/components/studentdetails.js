1d5e2699f0fde2c1db90ad037bee44fc7c7ac0d6
johnkennady <115686295+johnkennady220@users.noreply.github.com>
2023-01-13 17:19:52 +0530

    Add files via upload

---
+++ src/components/studentdetails.js
@@ -0,0 +1,27 @@
+const StudentDetails = [
+{   
+    id    :1,
+    name  :"Ajith",
+    age   :15,
+    gender:"male",
+    class :"8",
+    who   :'student'
+},
+{   
+    id    :2,
+    name  :"siva",
+    age   :15,
+    gender:"male",
+    class :"8",
+    who   :'student'
+},
+{   
+    id    :3,
+    name  :"john",
+    age   :3,
+    gender:"male",
+    class :"LKG",
+    who   :'student'
+},
+]
+export default StudentDetails ;
---
+++ src/components/teachersdetails.js
@@ -0,0 +1,28 @@
+const teachersData=[{
+    id   :301,
+    name : "jannu",
+    age  : 30,
+    gender:'female',
+    email: "jannu@xyz.com",
+    class: 9,
+    who  : 'teahcer'
+},{
+    id   : 302,
+    name : "akash",
+    age  : 32,
+    gender:'male',
+    email: "akash@xyz.com",
+    class: 9,
+    who  : 'teahcer'
+},{
+    id   : 303,
+    name : "mukesh",
+    age  : 29,
+    gender:'male',
+    email: "mukesh@xyz.com",
+    class: 7,
+    who  : 'teahcer'
+}
+]
+
+export default teachersData ;
